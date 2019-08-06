309a6a13a548d2d7cde1572868e3c0efd9eb060f
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2019-08-06 13:25:13 +0200

    Returns factorial of n ( n*(n-1)*(n-2)...*1)

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        private static BigInteger Factorial(int number)
+        {
+            var factorial = (BigInteger)1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
