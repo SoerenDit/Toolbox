7bf23675b0b136c98f7c4176be6c9acb098bb9e7
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2019-08-06 12:21:11 +0200

    Calculates all Fibonacci numbers until a certain value

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {     
+        private static List<int> Fibonacci(int highestFiboToBeFound)
+        {
+            var fibo = new List<int> { 1, 2 };
+            int i = 1;
+
+            while (fibo[i] <= highestFiboToBeFound)
+            {
+                i += 1;
+                fibo.Add(fibo[i - 2] + fibo[i - 1]);
+            }
+            return fibo;
+        }       
+    }
+}
