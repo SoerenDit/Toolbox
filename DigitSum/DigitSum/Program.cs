f1667878b5cc7c83f51892fc20804d04927b68dd
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2019-08-06 13:27:19 +0200

    Returns sum of digits in number

---
+++ DigitSum/DigitSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace DigitSum
+{
+    class Program
+    {
+        private static object DigitSum(BigInteger longNumber)
+        {
+            string text = longNumber.ToString();
+            var sum = 0;
+
+            foreach (char c in text)
+            {
+                sum += c - '0';
+            }
+            return sum;
+        }
+    }
+}
