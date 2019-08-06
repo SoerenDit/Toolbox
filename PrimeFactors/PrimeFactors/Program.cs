892e5d6f690dcc1d99439364ee99233635af2a66
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2019-08-06 12:26:33 +0200

    Calculates the prime factors of a number

---
+++ PrimeFactors/PrimeFactors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    class Program
+    {
+        private static List<int> PrimeFactor(long number)
+        {
+            var primeFactor = new List<int>();
+
+            int div = 2;
+            while (number != 1)
+            {
+                if (number % div == 0)
+                {
+                    primeFactor.Add(div);
+                    number = number / div;
+                    div--;
+                }
+                div++;
+            }
+            return primeFactor;
+        }
+    }
+}
