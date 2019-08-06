de5067fec2a237c10863ff494249646aac98b5dc
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2019-08-06 12:31:45 +0200

    Returns array of the first x primes

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        private static int[] PrimeNumbers(int xthPrime)
+        {
+            var primeNumbers = new int[xthPrime-1];
+
+            primeNumbers[0] = 2;
+            var n = 0;
+            var num = 3;
+
+            while (n < xthPrime - 1)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    if (num % primeNumbers[i] == 0)
+                    {
+                        break;
+                    }
+                    if (i == n)
+                    {
+                        n++;
+                        primeNumbers[n] = num;
+                    }
+                }
+                num += 2;
+            }
+            return primeNumbers;
+        }
+    }
+}
