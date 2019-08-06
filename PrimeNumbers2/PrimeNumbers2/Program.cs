1abd72b9b4794f7b2a0c91f1b45dc9bb618d6264
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2019-08-06 12:57:23 +0200

    Returns list of all primes below certain number

---
+++ PrimeNumbers2/PrimeNumbers2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers2
+{
+    class Program
+    {
+        private static List<double> PrimeNumbers2(double maxPrime)
+        {
+            var primeNumbers = new List<double> { 2 };
+            int num = 3;
+
+            while (num < maxPrime)
+            {
+                for (int i = 0; i <= primeNumbers.Count - 1; i++)
+                {
+                    if (num % primeNumbers[i] == 0)
+                    {
+                        break;
+                    }
+                    if (i == primeNumbers.Count - 1)
+                    {
+                        primeNumbers.Add(num);
+                    }
+                }
+                num += 2;
+            }
+            return primeNumbers;
+        }
+    }
+}
